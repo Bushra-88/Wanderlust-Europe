37961f759384a75598af9847eb44239b26127fe9
Bushra Alhusaini <alhusaini.b2023@gmail.com>
2024-09-13 12:23:08 +0200

    Create an export file for collection

---
+++ src/collection.js
@@ -0,0 +1,119 @@
+export const collection = [
+  {
+    name: "Paris, France",
+    description:
+      "The City of Love, Paris, is famous for its iconic landmarks such as the Eiffel Tower, Louvre Museum, and Notre-Dame Cathedral. Enjoy French cuisine, stroll along the Seine River, and immerse yourself in art and culture.",
+    location: "Île-de-France",
+    famousFeatures: ["Eiffel Tower", "Champs-Elysées", "Montmartre"],
+    bestTimeToVisit: "spring and fall",
+    link: "Learn more",
+    image: "Paris.jpg",
+  },
+
+  {
+    name: "Amsterdam, Netherland",
+    description:
+      "Amsterdam is known for its picturesque canals, historic architecture, and vibrant cultural scene. Visit the Anne Frank House, Rijks museum, and take a canal cruise.",
+    location: "North Holland",
+    famousFeatures: ["Dam Square", "Van Gogh Museum", "Jordaan district"],
+    bestTimeToVisit: "spring and summer",
+    link: " Learn more",
+    image: "Amsterdam.jpg",
+  },
+
+  {
+    name: "Barcelona, Spain",
+    description:
+      "Barcelona offers a blend of Gothic and modernist architecture. Explore Antoni Gaudí's masterpieces (Sagrada Família, Park Güell), relax on the beaches, and savor tapas",
+    location: "Catalonia",
+    famousFeatures: ["La Rambla", "Casa Batllo", "Gothic Quarter"],
+    bestTimeToVisit: "spring and autumn",
+    link: "Learn more",
+    image: "Barcelona.jpg",
+  },
+
+  {
+    name: "Rome, Italy",
+    description:
+      " The Eternal City, Rome, is a treasure trove of ancient history. Visit the Colosseum, Vatican City, and indulge in authentic Italian pasta and gelato.",
+    location: "Lazio",
+    famousFeatures: ["Colosseum", "Pantheon", "Trevi Fountain"],
+    bestTimeToVisit: "spring and autumn",
+    link: "Learn more",
+    image: "Rome.jpg",
+  },
+
+  {
+    name: "Vienna, Austria",
+    description:
+      "Vienna is a city of classical music, elegant palaces, and coffeehouses. Explore Schönbrunn Palace, Belvedere Palace, and attend a concert at the Vienna State Opera.",
+    location: "Vienna",
+    famousFeatures: ["Hofburg Palace", "St.Stephen's Cathedral", "Prater Park"],
+    bestTimeToVisit: "spring and summer",
+    link: "Learn more",
+    image: "Vienna.jpg",
+  },
+
+  {
+    name: "Prague, Czech Republic",
+    description:
+      "Prague, the City of a Hundred Spires, boasts a fairy-tale charm. Visit Prague Castle, Charles Bridge, and enjoy Czech beer in historic pubs.",
+    location: "Prague",
+    famousFeatures: ["Old Town Square", "Astronomical Clock", "Vysehrad"],
+    bestTimeToVisit: "spring and autumn",
+    link: "Learn more",
+    image: "Prague.jpg",
+  },
+
+  {
+    name: "Athens, Greece",
+    description:
+      "Athens is the cradle of Western civilization. Explore the Acropolis, Plaka neighborhood, and taste Greek souvlaki.",
+    location: "Attica",
+    famousFeatures: ["Parthenon", " Acropolis Museum", "Monastiraki Square"],
+    bestTimeToVisit: "spring and autumn",
+    link: "Learn more",
+    image: "Athens.jpg",
+  },
+
+  {
+    name: "Dubrovnik, Croatia",
+    description:
+      "Dubrovnik, the “Pearl of the Adriatic,” offers stunning medieval architecture. Walk the city walls, visit Fort Lovrijenac, and swim in the crystal-clear waters.",
+    location: "Dubrovnik-Neretva County",
+    famousFeatures: [" Old Town", "Stradun", "Lokrum Island"],
+    bestTimeToVisit: "spring and autumn",
+    link: "Learn more",
+    image: "Dubrovnik.jpg",
+  },
+
+  {
+    name: "Edinburgh, Scotland",
+    description:
+      "Edinburgh combines history, culture, and natural beauty. Explore Edinburgh Castle, the Royal Mile, and hike up Arthur's Seat.",
+    location: "Edinburgh",
+    famousFeatures: [
+      " Holyrood Palace",
+      "Princes Street Gardens",
+      "Calton Hill",
+    ],
+    bestTimeToVisit: " summer",
+    link: "Learn more",
+    image: "Edinburgh.jpg",
+  },
+
+  {
+    name: "London, United Kingdom",
+    description:
+      "London, the vibrant capital of the United Kingdom, seamlessly blends history, culture, and modernity. From iconic landmarks to hidden gems, there's something for every traveler.",
+    location: "London",
+    famousFeatures: [
+      "Buckingham Palace",
+      "The British Museum",
+      "The London Eye",
+    ],
+    bestTimeToVisit: "summer",
+    link: "Learn more",
+    image: "London.jpg",
+  },
+];
